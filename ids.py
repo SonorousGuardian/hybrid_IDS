a873c10bca15ca711e069287e8ac4b647c52d500
SonorousGuardian <97004910+SonorousGuardian@users.noreply.github.com>
2025-08-07 01:39:00 +0530

    Create ids.py

---
+++ ids.py
@@ -0,0 +1,308 @@
+import numpy as np
+import pandas as pd
+import seaborn as sns
+import missingno as msno
+sns.set(style='darkgrid')
+import matplotlib.pyplot as plt
+import pickle
+
+data1 = pd.read_csv('/content/Friday-WorkingHours-Afternoon-DDos.pcap_ISCX.csv')
+data2 = pd.read_csv('/content/Tuesday-WorkingHours.pcap_ISCX.csv')
+data3 = pd.read_csv('/content/Wednesday-workingHours.pcap_ISCX.csv')
+data4 = pd.read_csv('/content/Thursday-WorkingHours-Morning-WebAttacks.pcap_ISCX.csv')
+data5 = pd.read_csv('/content/Thursday-WorkingHours-Afternoon-Infilteration.pcap_ISCX.csv')
+data6 = pd.read_csv('/content/Friday-WorkingHours-Morning.pcap_ISCX.csv')
+data7 = pd.read_csv('/content/Friday-WorkingHours-Afternoon-PortScan.pcap_ISCX.csv')
+data8 = pd.read_csv('/content/Friday-WorkingHours-Afternoon-DDos.pcap_ISCX.csv')
+
+data_list = [data1, data2, data3, data4, data5, data6, data7, data8]
+
+data = pd.concat(data_list)
+rows, cols = data.shape
+
+
+# Deleting dataframes after concating to save memory
+for d in data_list: del d
+# Renaming the columns by removing leading/trailing whitespace
+col_names = {col: col.strip() for col in data.columns}
+data.rename(columns = col_names, inplace = True)
+data.columns
+
+dups = data[data.duplicated()]
+print(f'Number of duplicates: {len(dups)}')
+
+data.drop_duplicates(inplace = True)
+data.shape
+missing_val = data.isna().sum()
+print(missing_val.loc[missing_val > 0])
+
+# Checking for infinity values
+numeric_cols = data.select_dtypes(include = np.number).columns
+inf_count = np.isinf(data[numeric_cols]).sum()
+
+# Replacing any infinite values (positive or negative) with NaN (not a number)
+data.replace([np.inf, -np.inf], np.nan, inplace = True)
+missing = data.isna().sum()
+print(missing.loc[missing > 0])
+
+# Calculating missing value percentage in the dataset
+mis_per = (missing / len(data)) * 100
+mis_table = pd.concat([missing, mis_per.round(2)], axis = 1)
+mis_table = mis_table.rename(columns = {0 : 'Missing Values', 1 : 'Percentage of Total Values'})
+
+print(mis_table.loc[mis_per > 0])
+
+med_flow_bytes = data['Flow Bytes/s'].median()
+med_flow_packets = data['Flow Packets/s'].median()
+
+
+
+# Filling missing values with median
+data['Flow Bytes/s'].fillna(med_flow_bytes, inplace = True)
+data['Flow Packets/s'].fillna(med_flow_packets, inplace = True)
+
+
+data['Label'].unique()
+# Creating a dictionary that maps each label to its attack type
+attack_map = {
+    'BENIGN': 'BENIGN',
+    'DDoS': 'DDoS',
+    'DoS Hulk': 'DoS',
+    'DoS GoldenEye': 'DoS',
+    'DoS slowloris': 'DoS',
+    'DoS Slowhttptest': 'DoS',
+    'PortScan': 'Port Scan',
+    'FTP-Patator': 'Brute Force',
+    'SSH-Patator': 'Brute Force',
+    'Bot': 'Bot',
+    'Web Attack   Brute Force': 'Web Attack',
+    'Web Attack   XSS': 'Web Attack',
+    'Web Attack   Sql Injection': 'Web Attack',
+    'Infiltration': 'Infiltration',
+    'Heartbleed': 'Heartbleed'
+}
+# Creating a new column 'Attack Type' in the DataFrame based on the attack_map dictionary
+data['Attack Type'] = data['Label'].map(attack_map)
+data['Attack Type'].value_counts()
+
+data.drop('Label', axis = 1, inplace = True)
+
+from sklearn.preprocessing import LabelEncoder
+le = LabelEncoder()
+data['Attack Number'] = le.fit_transform(data['Attack Type'])
+print(data['Attack Number'].unique())
+# Printing corresponding attack type for each encoded value
+encoded_values = data['Attack Number'].unique()
+for val in sorted(encoded_values):
+    print(f"{val}: {le.inverse_transform([val])[0]}")
+
+corr = data.corr(numeric_only = True).round(2)
+corr.style.background_gradient(cmap = 'coolwarm', axis = None).format(precision = 2)
+
+# Positive correlation features for 'Attack Number'
+pos_corr_features = corr['Attack Number'][(corr['Attack Number'] > 0) & (corr['Attack Number'] < 1)].index.tolist()
+
+print("Features with positive correlation with 'Attack Number':\n")
+for i, feature in enumerate(pos_corr_features, start = 1):
+    corr_value = corr.loc[feature, 'Attack Number']
+    print('{:<3} {:<24} :{}'.format(f'{i}.', feature, corr_value))
+
+print(f'Number of considerable important features: {len(pos_corr_features)}')
+
+# Checking for columns with zero standard deviation (the blank squares in the heatmap)
+std = data.std(numeric_only = True)
+zero_std_cols = std[std == 0].index.tolist()
+zero_std_cols
+ # Data sampling for data analysis
+sample_size = int(0.2 * len(data)) # 20% of the original size
+sampled_data = data.sample(n = sample_size, replace = False, random_state = 0)
+sampled_data.shape
+
+data.drop('Attack Number', axis = 1, inplace = True)
+
+# Identifying outliers
+numeric_data = sampled_data.select_dtypes(include = ['float', 'int'])
+q1 = numeric_data.quantile(0.25)
+q3 = numeric_data.quantile(0.75)
+iqr = q3 - q1
+outlier = (numeric_data < (q1 - 1.5 * iqr)) | (numeric_data > (q3 + 1.5 * iqr))
+outlier_count = outlier.sum()
+outlier_percentage = round(outlier.mean() * 100, 2)
+outlier_stats = pd.concat([outlier_count, outlier_percentage], axis = 1)
+outlier_stats.columns = ['Outlier Count', 'Outlier Percentage']
+
+print(outlier_stats)
+
+# Identifying outliers based on attack type
+outlier_counts = {}
+for i in numeric_data:
+    for attack_type in sampled_data['Attack Type'].unique():
+        attack_data = sampled_data[i][sampled_data['Attack Type'] == attack_type]
+        q1, q3 = np.percentile(attack_data, [25, 75])
+        iqr = q3 - q1
+        lower_bound = q1 - 1.5 * iqr
+        upper_bound = q3 + 1.5 * iqr
+        num_outliers = ((attack_data < lower_bound) | (attack_data > upper_bound)).sum()
+        outlier_percent = num_outliers / len(attack_data) * 100
+        outlier_counts[(i, attack_type)] = (num_outliers, outlier_percent)
+
+for i in numeric_data:
+  print(f'Feature: {i}')
+  for attack_type in sampled_data['Attack Type'].unique():
+    num_outliers, outlier_percent = outlier_counts[(i, attack_type)]
+    print(f'- {attack_type}: {num_outliers} ({outlier_percent:.2f}%)')
+  print()
+
+data.groupby('Attack Type').first()
+
+old_memory_usage = data.memory_usage().sum() / 1024 ** 2
+print(f'Initial memory usage: {old_memory_usage:.2f} MB')
+for col in data.columns:
+    col_type = data[col].dtype
+    if col_type != object:
+        c_min = data[col].min()
+        c_max = data[col].max()
+        # Downcasting float64 to float32
+        if str(col_type).find('float') >= 0 and c_min > np.finfo(np.float32).min and c_max < np.finfo(np.float32).max:
+            data[col] = data[col].astype(np.float32)
+
+        # Downcasting int64 to int32
+        elif str(col_type).find('int') >= 0 and c_min > np.iinfo(np.int32).min and c_max < np.iinfo(np.int32).max:
+            data[col] = data[col].astype(np.int32)
+
+data.info()
+
+# Dropping columns with only one unique value
+num_unique = data.nunique()
+one_variable = num_unique[num_unique == 1]
+not_one_variable = num_unique[num_unique > 1].index
+
+dropped_cols = one_variable.index
+data = data[not_one_variable]
+
+# Standardizing the dataset
+from sklearn.preprocessing import StandardScaler
+
+features = data.drop('Attack Type', axis = 1)
+attacks = data['Attack Type']
+
+scaler = StandardScaler()
+scaled_features = scaler.fit_transform(features)
+with open('scaler.pkl', 'wb') as scaler_file:
+    pickle.dump(scaler, scaler_file)
+
+from sklearn.decomposition import IncrementalPCA
+
+size = len(features.columns) // 2
+ipca = IncrementalPCA(n_components = size, batch_size = 500)
+for batch in np.array_split(scaled_features, len(features) // 500):
+    ipca.partial_fit(batch)
+
+
+transformed_features = ipca.transform(scaled_features)
+new_data = pd.DataFrame(transformed_features, columns = [f'PC{i+1}' for i in range(size)])
+new_data['Attack Type'] = attacks.values
+
+
+# Creating a balanced dataset for Binary Classification
+normal_traffic = new_data.loc[new_data['Attack Type'] == 'BENIGN']
+intrusions = new_data.loc[new_data['Attack Type'] != 'BENIGN']
+
+normal_traffic = normal_traffic.sample(n = len(intrusions), replace = False)
+
+ids_data = pd.concat([intrusions, normal_traffic])
+ids_data['Attack Type'] = np.where((ids_data['Attack Type'] == 'BENIGN'), 0, 1)
+bc_data = ids_data.sample(n = 15000)
+
+# Splitting the data into features (X) and target (y)
+from sklearn.model_selection import train_test_split
+
+X_bc = bc_data.drop('Attack Type', axis = 1)
+y_bc = bc_data['Attack Type']
+
+X_train_bc, X_test_bc, y_train_bc, y_test_bc = train_test_split(X_bc, y_bc, test_size = 0.25, random_state = 0)
+
+new_data['Attack Type'].value_counts()
+
+class_counts = new_data['Attack Type'].value_counts()
+selected_classes = class_counts[class_counts > 1950]
+class_names = selected_classes.index
+selected = new_data[new_data['Attack Type'].isin(class_names)]
+
+dfs = []
+for name in class_names:
+  df = selected[selected['Attack Type'] == name]
+  if len(df) > 2500:
+    df = df.sample(n = 5000, random_state = 0)
+
+  dfs.append(df)
+
+df = pd.concat(dfs, ignore_index = True)
+df['Attack Type'].value_counts()
+
+
+from imblearn.over_sampling import SMOTE
+
+X = df.drop('Attack Type', axis=1)
+y = df['Attack Type']
+
+smote = SMOTE(sampling_strategy='auto', random_state=0)
+X_upsampled, y_upsampled = smote.fit_resample(X, y)
+
+blnc_data = pd.DataFrame(X_upsampled)
+blnc_data['Attack Type'] = y_upsampled
+blnc_data = blnc_data.sample(frac=1)
+
+blnc_data['Attack Type'].value_counts()
+
+features = blnc_data.drop('Attack Type', axis = 1)
+labels = blnc_data['Attack Type']
+
+X_train, X_test, y_train, y_test = train_test_split(features, labels, test_size = 0.25, random_state = 0)
+
+from sklearn.tree import DecisionTreeClassifier
+# For cross validation
+from sklearn.model_selection import cross_val_score
+dt1 = DecisionTreeClassifier(max_depth = 6)
+dt1.fit(X_train, y_train)
+
+cv_dt1 = cross_val_score(dt1, X_train, y_train, cv = 5)
+print('Decision Tree Model 1')
+print(f'\nCross-validation scores:', ', '.join(map(str, cv_dt1)))
+print(f'\nMean cross-validation score: {cv_dt1.mean():.2f}')
+
+dt2 = DecisionTreeClassifier(max_depth = 8)
+dt2.fit(X_train, y_train)
+
+cv_dt2 = cross_val_score(dt2, X_train, y_train, cv = 5)
+print('Decision Tree Model 2')
+print(f'\nCross-validation scores:', ', '.join(map(str, cv_dt2)))
+print(f'\nMean cross-validation score: {cv_dt2.mean():.2f}')
+
+# Sampling and shuffling the balanced dataset
+blnc_data = blnc_data.sample(frac=1.0, random_state=0).reset_index(drop=True)
+
+# Splitting into features (X) and target (y)
+X = blnc_data.drop('Attack Type', axis=1)
+y = blnc_data['Attack Type']
+
+# Splitting the data into train and test sets
+from sklearn.model_selection import train_test_split
+
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.25, random_state=42, stratify=y)
+
+# Building a Classification Model (e.g., Random Forest)
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.metrics import classification_report, confusion_matrix
+
+clf = RandomForestClassifier(n_estimators=100, random_state=0)
+clf.fit(X_train, y_train)
+with open('random_forest_model.pkl', 'wb') as model_file:
+    pickle.dump(clf, model_file)
+
+# Predictions and evaluation
+y_pred = clf.predict(X_test)
+
+print("Classification Report:\n", classification_report(y_test, y_pred))
+print("Confusion Matrix:\n", confusion_matrix(y_test, y_pred))
+
